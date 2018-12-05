afe6609e776ea02648a6c3eab5184e021040cc98
Joseph Kato <joseph@jdkato.io>
2018-12-05 11:28:00 -0800

    Add test for structure of `meta.json` files

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -5,18 +5,28 @@ import unittest
 import jsonschema
 
 
-META = pathlib.Path(__file__).parents[1] / "openpdi" / "meta"
+DATA = pathlib.Path(__file__).parents[1] / "openpdi" / "meta"
+
 SCHEMA = pathlib.Path(__file__).parents[0] / "schema.json"
+META = pathlib.Path(__file__).parents[0] / "meta.json"
 
 
 class SchemaTestCase(unittest.TestCase):
     """Ensure all schema.json files are valid.
     """
 
-    def test_valid(self):
-        for f in META.glob("**/schema.json"):
+    def test_datasets(self):
+        for f in DATA.glob("**/schema.json"):
             try:
                 with f.open() as e, SCHEMA.open() as s:
+                    jsonschema.validate(json.load(e), json.load(s))
+            except jsonschema.ValidationError as e:
+                self.fail(e)
+
+    def test_data_sources(self):
+        for f in DATA.glob("**/meta.json"):
+            try:
+                with f.open() as e, META.open() as s:
                     jsonschema.validate(json.load(e), json.load(s))
             except jsonschema.ValidationError as e:
                 self.fail(e)
